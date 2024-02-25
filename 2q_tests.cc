dc547c14bed1ee51b1b026a6d1c4482f6a5b6d4d
Привет Непривет <xmickos@MacBook-Pro-Privet.local>
2024-02-25 20:37:40 +0300

    Fifo completely debugged in isolated tests (see 2q_tests.cc)

--- 2q_tests.cc
+++ 2q_tests.cc
@@ -1,40 +1,72 @@
 #include "cache_me_if_you_can.hpp"
 #include <gtest/gtest.h>
+#include <random>
 
 #define TEST_SIZE 100
 
-TEST(Two_queues, BasicAssertions){
+TEST(Two_queues, Main_test){
 
-    caches::two_queues<int> my_2q(75, 25);
+    caches::two_queues<int> test_qs(3, 2);
 
-    int *input = new int[TEST_SIZE];
-    bool *results = new bool[TEST_SIZE];
+//     int *input = new int[TEST_SIZE];
+//     bool *results = new bool[TEST_SIZE];
+//
+//     for(int i = 0; i < TEST_SIZE; i++){
+//         input[i] = rand() % TEST_SIZE;
+//     }
+//
+//     int summ = 0;
+//
+//     for(int i = 0; i < TEST_SIZE; i++){
+//         results[i] = test_qs.cache_update(input[i]);
+//         summ += results[i];
+//     }
+//
+//     std::cout << "////////////////////\n\n";
+//
+//     std::cout << "Summ is " << summ << std::endl;
+//
+//     delete [] input;
+//     delete [] results;
 
-    for(int i = 0; i < TEST_SIZE; i++){
-        input[i] = rand() % TEST_SIZE;
+    std::default_random_engine generator;
+    std::binomial_distribution<int> distribution(1000,0.5);
+
+    int *bdistr_input = new int[TEST_SIZE];
+
+    for(int i = 0; i < TEST_SIZE; ++i){
+        bdistr_input[i] = distribution(generator);
     }
 
-    int summ = 0;
+    int b_hits = 0;
 
-    for(int i = 0; i < TEST_SIZE; i++){
-        results[i] = my_2q.cache_update(input[i]);
-        // std::cout << "i = " << i;
-        // if(results[i] == 1) std::cout << " + (" << summ << ")";
-        // std::cout << std::endl;
-        summ += results[i];
+    int input1[13] = {1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5};
+
+    for(int i = 0; i < TEST_SIZE; ++i){
+        b_hits += test_qs.cache_update(bdistr_input[i]);
+        std::cout << "\nAfter updating with " << bdistr_input[i] << " we have: " << std::endl <<
+        "Lru:" << std::endl;
+        for(auto it = test_qs.Am.lst_.begin(); it != test_qs.Am.lst_.end(); ++it){
+            std::cout << *it << " ";
+        }
+        std::cout << std::endl << "Ain1:" << std::endl;
+        for(auto it = test_qs.Ain1.lst_.begin(); it != test_qs.Ain1.lst_.end(); ++it){
+            std::cout << *it << " ";
+        }
+        std::cout << std::endl << "Ain2:" << std::endl;
+        for(auto it = test_qs.Ain2.lst_.begin(); it != test_qs.Ain2.lst_.end(); ++it){
+            std::cout << *it << " ";
+        }
+        std::cout << "\n\n\n";
     }
 
-    std::cout << "Summ is " << summ << std::endl;
+    std::cout << "Binominal distribution: " << b_hits <<  " HITS." << std::endl;
 
-    delete [] input;
-    delete [] results;
-
-    EXPECT_EQ(42, 42);
+    delete [] bdistr_input;
 
 }
 
-
-TEST(Two_queues, Fifo_isolated){
+TEST(Two_queues, DISABLED_Fifo_isolated){
 
     size_t test_size = 75, test_capacity = 25;
 
@@ -87,7 +119,7 @@ TEST(Two_queues, Fifo_isolated){
     std::cout << "//////Sizes: \n" << test_fifo.htable_.size() << " " << lst.size() << std::endl;
     std::cout << std::endl;
 
-    ASSERT_LE(test_fifo.htable_.size(), lst.size());
+    ASSERT_EQ(test_fifo.htable_.size(), lst.size());
     ASSERT_EQ(lst.size(), 9);
 
 
@@ -194,4 +226,72 @@ TEST(Two_queue, DISABLED_multimap){
 
     for (auto it = range.first; it != range.second; ++it)
         std::cout << it->first << ' ' << it->second << '\n';
+}
+
+TEST(Two_queues, DISABLED_LRU_isolated){
+
+    int test_size = 75, test_capacity = 25;
+    caches::lru_cache<int> test_lru{test_capacity};
+
+    EXPECT_EQ(test_lru.capacity_, test_capacity);
+    EXPECT_EQ(test_lru.htable_.size(), 0);
+    EXPECT_EQ(test_lru.lst_.size(), 0);
+
+    for(int i = 0; i < 9; ++i){
+        ASSERT_FALSE(test_lru.lru_update(i));
+    }
+
+    for(int i = 0; i < 9; ++i){
+        ASSERT_TRUE(test_lru.lru_update(i));
+    }
+
+    for(auto it = test_lru.lst_.begin(); it != test_lru.lst_.end(); ++it){
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    for(int i = 9; i < test_capacity; ++i){
+        test_lru.lru_update(i);
+    }
+
+    for(int i = test_capacity; i < test_capacity + 10; ++i){
+        test_lru.lru_update(i);
+    }
+
+    std::cout << "Current cache state: " << std::endl;
+    for(auto it = test_lru.lst_.begin(); it != test_lru.lst_.end(); ++it){
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    for(int i = 0; i < 10; ++i){
+        std::cout << "i = " << i << std::endl;
+        ASSERT_FALSE(test_lru.lru_update(i));
+    }
+
+    std::default_random_engine generator;
+    std::binomial_distribution<int> distribution(10000, 0.5);
+
+    int *bdistr_input = new int[TEST_SIZE];
+
+    for(int i = 0; i < TEST_SIZE; ++i){
+        bdistr_input[i] = distribution(generator);
+    }
+
+    for(int i = 0; i < 250; ++i){
+        std::cout << bdistr_input[i] << " ";
+        if(i % 35 == 0) { std::cout << std::endl; }
+    }
+    std::cout << std::endl;
+
+    int b_hits = 0;
+
+    for(int i = 0; i < TEST_SIZE; ++i){
+        b_hits += test_lru.lru_update(bdistr_input[i]);
+    }
+
+    std::cout << "Binomial distribution: " << b_hits << "." << std::endl;
+
+    delete [] bdistr_input;
+
 }
--- cache_me_if_you_can.hpp
+++ cache_me_if_you_can.hpp
@@ -20,24 +20,40 @@ struct lru_cache {
         std::cout << "Initialized lru_cache with capacity_ == " << capacity_ << std::endl;
     }
 
-    bool find_elem(T value) const{
+    bool find_elem(T value) const {
         return htable_.find(value) != htable_.end();
     }
 
-    void insert_elem(T value){
+    bool is_full() const {
+        return lst_.size() == capacity_;
+    }
+
+    void insert_elem(T value) {
         // если кэш полон, вытеснить последний элемент <-- ???
         // если в кэше есть указанный элемент, переместить его на первое место
 
         if(htable_.find(value) == htable_.end()){
+            std::cout << "Elem " << value << " not finded!" << std::endl;
+            if(is_full()){
+                auto it = lst_.back();
+                htable_.erase(it);
+                lst_.pop_back();
+            }
             lst_.push_front(value);
             htable_.insert({value, lst_.begin()});
         }else{
+            std::cout << "Elem " << value << " finded ! " << std::endl;
             auto it = htable_.at(value);
 
-            T tmp = *it;
-            lst_.erase(it);
-            lst_.push_front(tmp);
-            htable_.insert({tmp, lst_.begin()});
+            if(is_full()){
+                std::list<T> & lst_ref = lst_;
+                lst_.splice(lst_.begin(), lst_ref, it);
+            }else{
+                T tmp = *it;
+                lst_.erase(it);
+                lst_.push_front(tmp);
+                htable_.insert({tmp, lst_.begin()});
+            }
         }
     }
 
@@ -73,35 +89,35 @@ struct fifo {
 
     void insert_elem(T value) {
         if(is_full()){
-            std::cout << "Deleting " << lst_.back() << " in count of " << htable_.count(lst_.back()) << ", find was "
-                << find_elem(lst_.back());
+            // std::cout << "Deleting " << lst_.back() << " in count of " << htable_.count(lst_.back()) << ", find was "
+                // << find_elem(lst_.back());
 
             auto it_rng = htable_.equal_range(lst_.back());
 
             htable_.erase(it_rng.first);
             lst_.pop_back();
 
-            std::cout << ", new size is " << htable_.size() << std::endl;
+            // std::cout << ", new size is " << htable_.size() << std::endl;
         }
 
         lst_.push_front(value);
 
-        std::cout << "insert_elem(" << value << "), find_elem(" << value << ") = " << find_elem(value) <<
-            " (before inserting)";
+        // std::cout << "insert_elem(" << value << "), find_elem(" << value << ") = " << find_elem(value) <<
+            // " (before inserting)";
 
         htable_.insert({value, lst_.begin()});
 
-        std::cout << ", size = " << htable_.size() << ", find(" << value << ") = " << find_elem(value)
-            << "- after. Now is_full is " << is_full() << std::endl;
+        // std::cout << ", size = " << htable_.size() << ", find(" << value << ") = " << find_elem(value)
+            // << "- after. Now is_full is " << is_full() << std::endl;
     }
 
     void erase_elem(T value) {
         if(find_elem(value)){
-            std::cout << "finded elem " << value << " for erasing, trying to erase: ";
+            // std::cout << "finded elem " << value << " for erasing, trying to erase: ";
             auto range = htable_.equal_range(value);
-            if(range.first == htable_.end()){ std::cout << "zero range!"; }
+            // if(range.first == htable_.end()){ std::cout << "zero range!"; }
             for(auto it = range.first; it != range.second; ++it){
-                std::cout << it->first << " == " << *(it->second);
+                // std::cout << it->first << " == " << *(it->second);
                 lst_.erase(it->second);
             }
             htable_.erase(value);
@@ -128,9 +144,22 @@ struct two_queues {
     }
 
     void drag_between_fifos() {
+        std::cout << "Moving elem " << Ain1.lst_.back() << " from Ain1 to Ain2" << std::endl;
+
         T val = Ain1.lst_.back();
         Ain2.insert_elem(val);
         Ain1.erase_elem(val);
+
+        std::cout << "\nAin1:" << std::endl;
+        for(auto it = Ain1.lst_.begin(); it != Ain1.lst_.end(); ++it){
+            std::cout << *it << " ";
+        }
+        std::cout << std::endl;
+        std::cout << "Ain2:" << std::endl;
+        for(auto it = Ain2.lst_.begin(); it != Ain2.lst_.end(); ++it){
+            std::cout << *it << " ";
+        }
+        std::cout << std::endl;
     }
 
     void update_fifos(T value) {
